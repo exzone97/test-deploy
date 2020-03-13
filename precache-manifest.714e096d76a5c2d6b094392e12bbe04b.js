8345ba1e3748c746eea69afd1e4d6afa9f1fe530
Jonathan Alva <exzone97@yahoo.co.id>
2020-03-13 17:01:55 +0700

    Updates

---
+++ precache-manifest.714e096d76a5c2d6b094392e12bbe04b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbacf9a8e1ab5ee4d53b1a4302b0659",
+    "url": "/test-deploy/index.html"
+  },
+  {
+    "revision": "d7b5d40a8367ea820477",
+    "url": "/test-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c2e0d5bdc26632e22e3",
+    "url": "/test-deploy/static/js/2.9c3ff681.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/test-deploy/static/js/2.9c3ff681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b5d40a8367ea820477",
+    "url": "/test-deploy/static/js/main.2c1c3038.chunk.js"
+  },
+  {
+    "revision": "1ed1dee14d5e0461fc87",
+    "url": "/test-deploy/static/js/runtime-main.c051817f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
